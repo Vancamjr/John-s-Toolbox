cee82bf175ff1a85ca6451a917af249d9a16c3c7
Dr Van Camp <john@van-camp.com>
2018-05-21 22:39:33 -0700

    cleanup old files

--- hist_percent.R
+++
@@ -1,6 +0,0 @@
-hist_percent <- function(x, ...) {
-  H <- hist(x, plot = FALSE)
-  H$density <- with(H, 100 * density* diff(breaks)[1])
-  labs <- paste(round(H$density), "%", sep="")
-  plot(H, freq = FALSE, labels = labs, ylim=c(0, 1.08*max(H$density)),...)
-}
