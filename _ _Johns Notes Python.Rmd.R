a9386ad0fa007ee0a8433b4210432fca507e9e85
Dr Van Camp <john@van-camp.com>
2019-04-14 11:37:58 -0700

    Revert "Updates to notes"
    This reverts commit e833b706c56c351a39726eba8962797e5d562e1a.

--- _ _Johns Notes Python.Rmd.R
+++
@@ -1,8 +0,0 @@
-# Notes on R code
-Look up by section using the markdown chapter index
-
-```{r Delete all objects in current workspace}
-# rm(list = ls())
-# setwd("~/Documents/R/DVC Class")
-# rm(list=grep('modelFcst',ls(),value=T))  #remove all objects matching the pattern 'modelFcst'
-```
