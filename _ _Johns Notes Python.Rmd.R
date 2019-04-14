e833b706c56c351a39726eba8962797e5d562e1a
Dr Van Camp <john@van-camp.com>
2019-04-14 11:23:30 -0700

    Updates to notes

---
+++ _ _Johns Notes Python.Rmd.R
@@ -0,0 +1,8 @@
+# Notes on R code
+Look up by section using the markdown chapter index
+
+```{r Delete all objects in current workspace}
+# rm(list = ls())
+# setwd("~/Documents/R/DVC Class")
+# rm(list=grep('modelFcst',ls(),value=T))  #remove all objects matching the pattern 'modelFcst'
+```
